a23f762663695a44510a174c7be23b7e34fa66c3
François <francois.guechoum@gmail.com>
2026-08-01 13:48:14 +0200

    new contrib interface IFusioner IExploiter

---
+++ src/Domain/Espluque.Contracts/ModuleInterfaces/Contributions/IFusioner.cs
@@ -0,0 +1,9 @@
+﻿using Espluque.Contracts.Entities;
+
+namespace Espluque.Contracts.ModuleInterfaces.Contributions
+{
+    public interface IFusioner
+    {
+        Task<List<KeyValuePair<string, string>>> Fuse(AnalysisContext analysisContext);
+    }
+}
